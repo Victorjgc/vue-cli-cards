1319d0de35b12d69b01982ab10bde640afe1a25f
Victor Javier <VictorJavierGuamnChulunchana@gmail.com>
2019-11-22 20:29:41 +0000

    vue cli

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <BaseCard nickname="scar"></BaseCard>
+    <BaseCard nickname="wolf"></BaseCard>
+    <button class="btn" @click="showCard">Press</button>
+    <!--BaseCard v-for="u in chars" :key="u">
+      {{ u.chars }}
+    </BaseCard-->
+  </div>
+</template>
+
+<script>
+import BaseCard from "@/components/BaseCard.vue";
+export default {
+  name: "app",
+  components: {
+    BaseCard
+  },
+  methods: {
+    showCard() {
+      alert("You!");
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background: #4187b5;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  border: 1px solid blue;
+  display: flex;
+  justify-content: center;
+}
+.btn {
+  width: 100px;
+  height: 50px;
+}
+</style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="card" @click="fliped">
+    <div class="name">{{ chars[nickname].name }}</div>
+    <div class="strong">{{ chars[nickname].strong }}</div>
+    <img class="image" :src="chars[nickname].image" alt="" />
+    <div class="description">{{ chars[nickname].description }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BaseCard",
+  data() {
+    return {
+      image: "",
+      description: "",
+      counter: 0,
+      chars: {
+        scar: {
+          name: "Scar",
+          image: require("../assets/scar.jpg"),
+          strong: "Strong: 9",
+          description: "Just 1 eye to kill"
+        },
+        wolf: {
+          name: "Wolf",
+          strong: "Strong 6",
+          image: require("../assets/wolf.jpg"),
+          description: "I'm the strongest"
+        }
+      }
+    };
+  },
+  methods: {
+    fliped() {
+      console.log(this.$el);
+      this.$el.classList.toggle("fliped");
+    }
+  },
+  props: {
+    nickname: String
+  }
+};
+</script>
+
+<style>
+.card {
+  background: green;
+  margin: 10px;
+  align-self: center;
+
+  flex-direction: column;
+  border: 1px solid yellow;
+  background: white;
+}
+.image {
+  width: 300px;
+  height: 450px;
+  border: 2px solid black;
+  margin: 5px;
+  background-position: center;
+  background-repeat: no-repeat;
+  -webkit-box-shadow: 14px 13px 0px -9px rgba(0, 0, 0, 0.74);
+  -moz-box-shadow: 14px 13px 0px -9px rgba(0, 0, 0, 0.74);
+  box-shadow: 14px 13px 0px -9px rgba(0, 0, 0, 0.74);
+}
+.fliped {
+  background: red;
+}
+.fliped * {
+  visibility: hidden;
+}
+</style>
